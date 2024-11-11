7267ca5914b107ee9a3045fedeec77caab6f9a1f
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2024-11-10 23:29:31 -0500

    Add ICInvalidateCache declaration

--- include/dolphin/OSCache.h
+++ include/dolphin/OSCache.h
@@ -5,6 +5,7 @@
 
 void LCEnable();
 void DCInvalidateRange(void *addr, u32 nBytes);
+void ICInvalidateRange(void *addr, u32 nBytes);
 void DCFlushRange(void *addr, u32 nBytes);
 void DCStoreRange(void *addr, u32 nBytes);
 
